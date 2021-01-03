1bbad90deb0dc5b9940daa72755e1f4ae81cabd6
Dan Soutar <dan.soutar@littlerocket.io>
2021-01-03 18:17:46 -0500

    finished project

--- scripts.js
+++ scripts.js
@@ -16,13 +16,55 @@ myImage.addEventListener('load', () => {
   const ctx = canvas.getContext('2d');
   // set canvas dimensions = to image dimensions
   canvas.width = 435;
-  canvas.width = 574;
+  canvas.height = 574;
 
   // canvas method to drag image to screen
   ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+  // get pixel data by scanning an area on the canvas
+  const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  // clear original image from canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 
   // setup array for particles
   let particlesArray = [];
+
+  // Get brightness map of the image
+  // create an array to hold pixel color data 
+  let mappedImage = [];
+  // create a loop starting at the first row
+  for (let y = 0; y < canvas.height; y++) {
+    // create an array for each row
+    let row = [];
+    // create a loop for every pixel across the row
+    for (let x = 0; x < canvas.width; x++) {
+      // get the rgb color data for each pixel
+      const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
+      const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
+      const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+      // calculate the brightness of the pixel
+      const brightness = calculateRelativeBrightness(red, green, blue);
+      // create a 'cell' variable representing each pixel that holds the pixel's the info we want to access 
+      const cell = [
+        cellBrightness = brightness,
+      ];
+      // add the cell (pixel) variable to the row array createds for this row 
+      row.push(cell);
+    }
+    // add the current row into the mappedImage array
+    mappedImage.push(row);
+  }
+
+  // custom function to calc brightness of colors based on human perception
+  function calculateRelativeBrightness(red, green, blue) {
+    return ( 
+      Math.sqrt(
+        (red * red) * 0.299 +
+        (green * green) * 0.587 +
+        (blue * blue) * 0.114
+      )/100
+    )
+  }
 
   // setup Particle
   class Particle {
@@ -34,15 +76,28 @@ myImage.addEventListener('load', () => {
       // initial speed is 0
       this.speed = 0;
       // particles move at different velocity
-      this.velocity = Math.random() * 3.5;
+      this.velocity = Math.random() * .8;
       // particles have varying sizes
-      this.size = Math.random() * 1.5 + 1;
+      this.size = Math.random() * 0.5 + 1;
+      // make sure particle coords are integers
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
     }
     update() {
       // - make the particles move downwards on the screen.
       // - when the particles reach the end of the screen,  start them back at the top
       //   at a random point on the x axis
-      this.y += this.velocity;
+
+      // round down coordinate values
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+
+      // calculate the speed of the particle based on its brightness
+      this.speed = mappedImage[this.position1][this.position2][0];
+
+      let movement = (2.5 - this.speed) + this.velocity;
+
+      this.y += movement;
       if (this.y >= canvas.height) {
         this.y = 0;
         this.x = Math.random() * canvas.width;
@@ -52,7 +107,7 @@ myImage.addEventListener('load', () => {
     draw() {
       // draw a circle for each particle
       ctx.beginPath();
-      ctx.fillStyle = 'white';
+      ctx.fillStyle = '#1b00f5';
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fill();
     }
@@ -67,16 +122,15 @@ myImage.addEventListener('load', () => {
   init();
 
   function animate() {
-    // canvas method to drag image to screen
-    ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
-    
     // draw a semi-transparent black bg each frame to give fading effect
     ctx.globalAlpha = 0.05;
     ctx.fillStyle = 'rgb(0,0,0)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 0.2;
     // update and draw each particle on the canvas every frame
     for (let i = 0; i <= numberOfParticles; i++) {
       particlesArray[i].update();
+      ctx.globalAlpha = particlesArray[i].speed*0.1;
       particlesArray[i].draw();
     }
     // loop animation
